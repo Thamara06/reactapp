1a3f12f74d7557cbfc7385a5ae7c00243bbc354f
Your Name <thamamishel17@gmail.com>
2023-06-23 13:14:58 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,10 +18,30 @@ function App() {
         son la maderera y agrícola, le merecen el nombre con el que se le conoce "Tierra de la Canela", fue una de las primeras zonas en colonizarse y donde los asentamientos pudieron iniciar la exportación de
         especies y productos agrícolas que los primeros expedicionarios se aventuraron a sembrar.
         </p>
-        <p>
-          <code> HISTORIA </code> 
-        </p>
         
+        <p>
+          <code> HISTORIA </code>
+        </p>
+        Presencia de los Incas en la zona del Napo.
+        <p>
+        <p>
+        Los cronistas españoles e historiadores identifican al menos dos grandes grupos humanos que habitaron la actual provincia de Napo, los Omaguas y los Quijos. En el caso de los primeros, parece ser que antes 
+        del encuentro con los españoles, los Omaguas ya habían tenido al menos un contacto con el Inca Huayna Cápac. Toribio de Ortigueira obtiene esta información a través de Isabel Guachi, quien estuvo en la expedición 
+        del Inca, dando la siguiente descripción: “eran de buena disposición, bien ajustados, vestidos de manta y camisas del algodón pintadas de pincel y de diferentes pinturas”. En el caso de los Quijos, era evidente que 
+        su contacto con los pueblos de la sierra interandina era muy fluido al igual que con los Incas.
+        </p>
+        <p>
+        Autores mencionan que la zona de Papallacta era un centro de sanación y paso obligado para ingresar a tierras bajas. Igualmente se menciona que los Incas destinaron funcionarios de su administración para que establezcan 
+        mecanismo de gobierno con los habitantes de las tierras cálidas. Gutiérrez Marín en su libro Quijos (2009) menciona que la zona de Quijos o “Hatunquixos” era conocida por estar ubicada al otro lado del gran río, y de la 
+        cual se podía aprovisionar de canela, oro, entre otros productos.En el mismo estudio se menciona a Pablo Ospina quien manifiesta que el territorio Quijo se encontraba situado al Norte con el río Aguarico, al Sur con el Napo, 
+        al Este el territorio Omagua, entre los ríos Coca y Napo, sobre todo hacia la cordillera, en la llamada anticlinal del Napo.
+        </p>
+        <p>
+        En el mismo estudio se menciona a Pablo Ospina quien manifiesta que el territorio Quijo se encontraba situado al Norte con el río Aguarico, al Sur con el Napo, al Este el territorio Omagua, entre los ríos Coca y Napo, sobre 
+        todo hacia la cordillera, en la llamada anticlinal del Napo.
+        </p>
+        </p>
+
       </header>
     </div>
   );
