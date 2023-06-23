225d5755684452dce11962d894f3fc01248429ca
Your Name <thamamishel17@gmail.com>
2023-06-23 12:35:21 -0500

    change

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           NAPO UN <code> PARAISO </code> DE MARAVILLAS
         </p>
+        <p>
         La provincia de Napo es una de las provincias de la Región Centro Norte (Ecuador), de la República del Ecuador, situada en la región amazónica ecuatoriana e incluyendo parte de las laderas de los Andes,
         hasta las llanuras amazónicas. Toma su nombre del río Napo. Su capital es la ciudad de Tena. Limita al norte con Sucumbíos, al sur con Pastaza, al oeste con Pichincha, Cotopaxi y Tungurahua y al este con la
         Provincia de Orellana. La provincia está conformada por cinco cantones, cinco parroquias urbanas y veinte parroquias rurales.Napo es territorio de dos majestuosas nacionalidades muy importantes del oriente
         del Ecuador, son los quichuas amazónicos y los huaoranis. Su producción se basa en el comercio de recursos de la zona, el turismo, la ganadería y agricultura. Las grandes industrias de la extracción como lo
         son la maderera y agrícola, le merecen el nombre con el que se le conoce "Tierra de la Canela", fue una de las primeras zonas en colonizarse y donde los asentamientos pudieron iniciar la exportación de
         especies y productos agrícolas que los primeros expedicionarios se aventuraron a sembrar.
-        <p>
-
         </p>
+        <p>
+          <code> HISTORIA </code> 
+        </p>
+        
       </header>
     </div>
   );
