bb136637b3580bdca160e44c3d991b466a47869a
Your Name <thamamishel17@gmail.com>
2023-06-23 11:27:36 -0500

    comind

--- src/NavItems/Table.js
+++ src/NavItems/Table.js
@@ -1,52 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import MyTable from "../MyTable";
 const Table = () => {
-
-  const [tableXY, setTableXY] = useState({});
-  
-
-  const handleChange = ({target}) =>{
-    const {name, value} = target;
-    setTableXY((prev) => ({
-      ...prev,
-      [name]:value
-    }));
-
-  }
-
-  
-
   return (
     <div>
-      <div>
-        <form id='tableForm' >
-          <label className='tableLabel'> How many columns?</label>
-          <span>
-            <input
-              className='tableInput'
-              type='number'
-              value={tableXY.nColumns}
-              name='nColumns'
-              placeholder='Enter the number of Columns'
-              onChange={handleChange}
-            ></input>
-          </span>
-          <label className='tableLabel'> How many rows?</label>
-          <span>
-            <input
-              className='tableInput'
-              type='number'
-              value={tableXY.nRows}
-              name='nRows'
-              placeholder='Enter the number of Rows'
-              onChange={handleChange}
-            ></input>
-          </span>
-          
-        </form>
-      </div>
-      <MyTable nColumns={tableXY.nColumns} nRows={tableXY.nRows} />
-      
+      <MyTable nColumns={100} nRows={65} />
+      <MyTable nColumns={15} nRows={90} />
+      <MyTable nColumns={100} nRows={5} />
     </div>
   );
 };
