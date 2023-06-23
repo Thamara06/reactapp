5df327e5a8f568db7c4e0ccf59f571037eb271b7
Your Name <thamamishel17@gmail.com>
2023-06-23 14:54:47 -0500

    commint chang

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
 
         <p>
-          NAPO UN <code> PARAISO </code> DE MARAVILLAS
+        <code>  NAPO UN PARAISO DE MARAVILLAS</code> 
         </p>
+
         <p>
         La provincia de Napo es una de las provincias de la Región Centro Norte (Ecuador), de la República del Ecuador, situada en la región amazónica ecuatoriana e incluyendo parte de las laderas de los Andes,
         hasta las llanuras amazónicas. Toma su nombre del río Napo. Su capital es la ciudad de Tena. Limita al norte con Sucumbíos, al sur con Pastaza, al oeste con Pichincha, Cotopaxi y Tungurahua y al este con la
@@ -18,12 +19,26 @@ function App() {
         son la maderera y agrícola, le merecen el nombre con el que se le conoce "Tierra de la Canela", fue una de las primeras zonas en colonizarse y donde los asentamientos pudieron iniciar la exportación de
         especies y productos agrícolas que los primeros expedicionarios se aventuraron a sembrar.
         </p>
+
+        <center><img
+            className='smallImage'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Tena5.jpg/300px-Tena5.jpg'alt="" width="300" height="190" 
+          />
+        <img
+            className='smallImage'
+            src='https://www.hotelcontact.net/image/ecuador/napo/tena/hotel-cruscaspi-tena-1607619517.jpg'alt="" width="300" height="190" 
+          />
         
+        <img
+            className='smallImage'
+            src='https://1.bp.blogspot.com/-qiIQ1CVcoCw/VGKGylcMghI/AAAAAAAAAQs/uMjWU2G-Ayw/s1600/6.%2BTena%2B-%2BMonumento%2Ba%2BJumandy%2B(Mintur).jpg' alt="" width="300" height="190" 
+          /> </center>
+
         <p>
           <code> HISTORIA </code>
         </p>
         Presencia de los Incas en la zona del Napo.
-        <p>
+        
         <p>
         Los cronistas españoles e historiadores identifican al menos dos grandes grupos humanos que habitaron la actual provincia de Napo, los Omaguas y los Quijos. En el caso de los primeros, parece ser que antes 
         del encuentro con los españoles, los Omaguas ya habían tenido al menos un contacto con el Inca Huayna Cápac. Toribio de Ortigueira obtiene esta información a través de Isabel Guachi, quien estuvo en la expedición 
@@ -40,11 +55,76 @@ function App() {
         En el mismo estudio se menciona a Pablo Ospina quien manifiesta que el territorio Quijo se encontraba situado al Norte con el río Aguarico, al Sur con el Napo, al Este el territorio Omagua, entre los ríos Coca y Napo, sobre 
         todo hacia la cordillera, en la llamada anticlinal del Napo.
         </p>
+        
+        <center><img
+            className='smallImage'
+            src='https://journals.openedition.org/revestudsoc/docannexe/image/47627/img-14-small580.jpg'alt="" width="300" height="190" 
+          />
+        <img
+            className='smallImage'
+            src='https://www.culturaypatrimonio.gob.ec/wp-content/uploads/2022/08/Alfareri%CC%81a-Napo-01-scaled.jpg'alt="" width="300" height="190" 
+          />
+        
+        <img
+            className='smallImage'
+            src='https://cdn.audleytravel.com/3988/2848/79/482218-napo-wildlife-centre.jpg' alt="" width="300" height="190" 
+          /> </center>
+
+        <p>
+          <code> SIMBOLOS PATRIOS DE NAPO </code>
+        </p>
+        <p> 
+          ESCUDO DE NAPO
+        </p>
+        <img
+            className='smallImage'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Escudo_de_la_Provincia_de_Napo.svg/1830px-Escudo_de_la_Provincia_de_Napo.svg.png' width="200" height= "200" 
+          /> 
+        
+        <p> 
+          BANDERA DE NAPO
+        </p>
+        <img
+            className='smallImage'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Bandera_Provincia_Napo.svg/1200px-Bandera_Provincia_Napo.svg.png' width="250" height= "200" 
+          /> 
+        <p>
+          <code> HIMNO DE NAPO </code>
+        </p>
+        <p>
+        <center>Y Dios creó esta tierra esplendorosa y bella</center>
+        <center>de Napo majestuoso ceñida en su extensión,</center>
+        <center>con el rumor y trinos de la bravía selva,</center>
+        <center>con quejas de fontanas, con el fulgor del sol</center>
+        </p>
+
+        <p>Estrofas</p>
+        <p>I</p>
+
+        <p>
+        <center>¡Napo! ¡Napo! Región prometida</center>
+        <center>como es glauca y feroz tu montaña</center>
+        <center>y a raudales brotó de su entraña</center>
+        <center>oro negro que el mito venció;</center>
+        <center>y entre hosanas y glorias la Patria,</center>
+        <center>te saluda en unción reverente</center>
+        <center>como a estrella que anuncia por </center>
+        <center>siempre</center>
+        <center>la alborada de un nuevo Ecuador.</center>
+        </p>
+
+        <p>II</p>
+        
+        <p>
+        <center>¡Salve! ¡Gloria! </center>
+        <center>¡Salve oh Napo, Oriente Ecuador! </center>
         </p>
 
       </header>
     </div>
   );
 }
+
+
 
 export default App;
